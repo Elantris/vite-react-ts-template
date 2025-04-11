2d08d804229ca9569dd73d27511fbd2a29cb863a
Elantris <j88107333@gmail.com>
2025-04-11 12:50:01 +0800

    Use wouter instead of react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import { RouterProvider } from 'react-router'
-import router from './router'
+import { Route, Switch } from 'wouter'
+import Layout from './components/Layout'
+import About from './pages/About'
+import Home from './pages/Home'
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Layout>
+  )
 }
 
 export default App
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,18 @@
-import { Link, Outlet } from 'react-router'
+import { FC, ReactNode } from 'react'
+import { Link } from 'wouter'
 
-const Layout = () => {
+const Layout: FC<{
+  children?: ReactNode
+}> = ({ children }) => {
   return (
-    <div>
-      <div className="flex gap-4">
+    <>
+      <nav className="flex gap-4 text-blue-500">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-      </div>
+      </nav>
 
-      <Outlet />
-    </div>
+      {children}
+    </>
   )
 }
 
